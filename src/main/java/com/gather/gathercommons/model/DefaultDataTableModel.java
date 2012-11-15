1347a7d04c74988278a3c48d0a891490a5c9910c
rtroy <rtroy@13f10598-5c34-4a87-bf25-6f6a81c8df7a>
2012-11-15 22:08:51 +0000

    FORMATO CODIGO
    git-svn-id: https://gathercl.svn.cloudforge.com/java/maven/gathercommons@8386 13f10598-5c34-4a87-bf25-6f6a81c8df7a

--- src/main/java/com/gather/gathercommons/model/DefaultDataTableModel.java
+++ src/main/java/com/gather/gathercommons/model/DefaultDataTableModel.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class DefaultDataTableModel implements IDataTableModel {
-
     private List<Object> selectedRow;
 
     private List<List<Object>> titles;
