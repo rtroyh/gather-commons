5adbbdcc909f5fe396338c24a0c063aee8b6ad8b
rtroy <rtroy@13f10598-5c34-4a87-bf25-6f6a81c8df7a>
2014-08-26 20:51:51 +0000

    MAPUTIL - CORRECCION IMPRESION DE VARIABLES
    git-svn-id: https://gathercl.svn.cloudforge.com/java/maven/gathercommons@10382 13f10598-5c34-4a87-bf25-6f6a81c8df7a

--- src/main/java/com/gather/gathercommons/util/MapUtil.java
+++ src/main/java/com/gather/gathercommons/util/MapUtil.java
@@ -2,7 +2,6 @@ package com.gather.gathercommons.util;
 
 import com.gather.gathercommons.util.interfaces.IMapUtil;
 
-import java.math.BigDecimal;
 import java.util.Map;
 
 public class MapUtil implements IMapUtil {
@@ -10,23 +9,18 @@ public class MapUtil implements IMapUtil {
     public String convertTOstring(Map<String, Object> mapa) throws
                                                             RuntimeException {
         if (mapa != null) {
-
             StringBuilder sb = new StringBuilder(mapa.size() * 2);
             sb.append("(");
 
             for (Object o : mapa.values()) {
-                if (o instanceof String) {
-                    sb.append("'");
-                    sb.append(o);
-                    sb.append("'");
-                } else if (o instanceof Integer ||
-                        o instanceof Double ||
-                        o instanceof Short ||
-                        o instanceof Long ||
-                        o instanceof BigDecimal) {
-                    sb.append(o);
-                } else if (o == null) {
+                if (o == null) {
                     sb.append("null");
+                } else if (o instanceof String) {
+                    sb.append("'");
+                    sb.append(o);
+                    sb.append("'");
+                } else {
+                    sb.append(o.toString());
                 }
 
                 sb.append(",");
