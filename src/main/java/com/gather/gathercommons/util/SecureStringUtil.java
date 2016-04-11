02344314b10445c264b947cf48783134216e1cf8
rtroy <rtroyh@gather.cl>
2016-04-11 15:48:44 -0300

    CLASE PARA GENERAR STRING DE OBJETOS

---
+++ src/main/java/com/gather/gathercommons/util/SecureStringUtil.java
@@ -0,0 +1,30 @@
+package com.gather.gathercommons.util;
+
+import java.util.List;
+
+/**
+ * Created with IntelliJ IDEA.
+ * $ Project: gather-commons
+ * User: rodrigotroy
+ * Date: 11-04-16
+ * Time: 15:45
+ */
+public class SecureStringUtil {
+
+    public static String objectToString(Object o) {
+        if (o != null) {
+            return o.toString().trim();
+        }
+
+        return "";
+    }
+
+    public static String cellRowToString(List<Object> row,
+                                         Integer cellIndex) {
+        if (Validator.validateList(row) && row.size() > cellIndex) {
+            return row.get(cellIndex).toString().trim();
+        }
+
+        return "";
+    }
+}
