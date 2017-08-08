6977ad83f90327bfa2fd240d0e1a340437b9ec22
rtroy <rtroyh@gather.cl>
2017-08-08 12:00:27 -0400

    NUEVA INTERFACE PARA ENUMS

---
+++ src/main/java/com/gather/gathercommons/model/IEnum.java
@@ -0,0 +1,15 @@
+package com.gather.gathercommons.model;
+
+/**
+ * Created with IntelliJ IDEA.
+ * $ Project: gather-commons
+ * User: rodrigotroy
+ * Date: 8/8/17
+ * Time: 11:56
+ */
+public interface IEnum {
+    /**
+     * @return un Integer unico para el grupo al cual pertenece el enum
+     */
+    Integer getID();
+}
