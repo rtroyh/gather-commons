c950dd17df97663d6a376f5d3b1a819a0429171e
rtroy <rtroyh@gather.cl>
2017-11-14 15:02:11 -0300

    CONSTRUCCION CLASE PARA MANIPULAR CAMPOS RUT

--- src/main/java/com/gather/gathercommons/rut/RUTHolder.java
+++ src/main/java/com/gather/gathercommons/rut/RUTHolder.java
@@ -1,14 +1,18 @@
 package com.gather.gathercommons.rut;
 
+import com.gather.gathercommons.util.DigitoVerificador;
+import com.gather.gathercommons.util.Validator;
+import org.apache.commons.lang3.StringUtils;
+
 /**
  * Created with IntelliJ IDEA.
  * $ Project: gathercommons
  * User: rodrigotroy
  * Date: 11/14/17
  * Time: 12:49
+ * Clase para manipular campos RUT que vienen desde el origen con DV.
  */
 public class RUTHolder {
-    private String rutOrigen;
     private String rutSinDV;
     private String rutConDV;
     private String digitoVerificador;
@@ -16,32 +20,61 @@ public class RUTHolder {
     private Boolean origenTieneDV;
     private Boolean dvEsVerificable;
 
+    public RUTHolder(String rutConDV) throws
+                                      RUTInvalidoException {
+        if (Validator.validateRUT(rutConDV)) {
+            this.rutConDV = rutConDV;
+            this.rutSinDV = rutConDV.substring(0,
+                                               rutConDV.indexOf("-")).replaceAll("\\.",
+                                                                                 "");
+            this.digitoVerificador = rutConDV.substring(rutConDV.indexOf("-") + 1,
+                                                        rutConDV.indexOf("-") + 2).toUpperCase();
+        } else {
+            throw new RUTInvalidoException("RUT ES INVALIDO");
+        }
+    }
 
-    public RUTHolder(String rutOrigen) {
-        this.rutOrigen = rutOrigen;
+    public String getDigitoVerificadorCalculado() {
+        return DigitoVerificador.calcular(Integer.parseInt(this.getRutSinDV()));
+    }
+
+    public Boolean esValido() {
+        Boolean rutEsValido;
+
+        if (Validator.validateRUT(this.rutConDV)) {
+            rutEsValido = StringUtils.isNumeric(rutSinDV);
+
+            if (!DigitoVerificador.calcular(Integer.parseInt(rutSinDV)).equals(digitoVerificador)) {
+                rutEsValido = false;
+            }
+        } else {
+            rutEsValido = true;
+        }
+
+        return rutEsValido;
     }
 
     public String getRutSinDV() {
+        if (rutSinDV == null) {
+
+        }
+
         return rutSinDV;
     }
 
-    public void setRutSinDV(String rutSinDV) {
-        this.rutSinDV = rutSinDV;
-    }
-
     public String getRutConDV() {
+        if (rutConDV == null) {
+
+        }
+
         return rutConDV;
     }
 
-    public void setRutConDV(String rutConDV) {
-        this.rutConDV = rutConDV;
-    }
-
     public String getDigitoVerificador() {
-        return digitoVerificador;
-    }
+        if (digitoVerificador == null) {
 
-    public void setDigitoVerificador(String digitoVerificador) {
-        this.digitoVerificador = digitoVerificador;
+        }
+
+        return digitoVerificador;
     }
 }
---
+++ src/main/java/com/gather/gathercommons/rut/RUTInvalidoException.java
@@ -0,0 +1,14 @@
+package com.gather.gathercommons.rut;
+
+/**
+ * Created with IntelliJ IDEA.
+ * $ Project: gathercommons
+ * User: rodrigotroy
+ * Date: 11/14/17
+ * Time: 14:56
+ */
+public class RUTInvalidoException extends Exception {
+    public RUTInvalidoException(String message) {
+        super(message);
+    }
+}
