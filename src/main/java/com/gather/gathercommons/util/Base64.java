8499d234534f025527d7de88ec8c117b7ae15a60
rtroy <rtroyh@gather.cl>
2017-11-17 12:03:45 -0300

    NUEVO METODO PARA GENERAR BASE64 DESDE UN BYTE[]

--- src/main/java/com/gather/gathercommons/util/Base64.java
+++ src/main/java/com/gather/gathercommons/util/Base64.java
@@ -21,19 +21,8 @@ public class Base64 {
         return new String(encoder.encode(text.getBytes()));
     }
 
-    public static String encode(byte[] src,
-                                boolean urlSafe) {
-        java.util.Base64.Encoder encoder;
-
-        if (urlSafe) {
-            encoder = java.util.Base64.getUrlEncoder();
-        } else {
-            encoder = java.util.Base64.getEncoder();
-        }
-
-        final byte[] encode1 = encoder.encode(src);
-
-        return new String(encode1);
+    public static String encodeByteToString(byte[] src) {
+        return org.apache.commons.codec.binary.Base64.encodeBase64String(src);
     }
 
     public static String decode(String text,
@@ -48,4 +37,6 @@ public class Base64 {
 
         return new String(decoder.decode(text.getBytes()));
     }
+
+
 }
